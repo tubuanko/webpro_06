fd921abcb2c9ad6b6c582612685941d08fac51ac
tubuanko <tanishi0320@icloud.com>
2024-11-04 23:17:13 +0900

    勝ち負け判定を加えた

--- app5.js
+++ app5.js
@@ -38,10 +38,22 @@ app.get("/janken", (req, res) => {
   else if( num==2 ) cpu = 'チョキ';
   else cpu = 'パー';
   // ここに勝敗の判定を入れる
-  // 今はダミーで人間の勝ちにしておく
-  let judgement = '勝ち';
-  win += 1;
+  
+  let judgement = '';
+  if (hand === cpu) {
+    judgement = '引き分け';
+  } else if (
+    (hand === 'グー' && cpu === 'チョキ') ||
+    (hand === 'チョキ' && cpu === 'パー') ||
+    (hand === 'パー' && cpu === 'グー')
+  ) {
+    judgement = '勝ち';
+    win += 1;
+  } else {
+    judgement = '負け';
+  }
   total += 1;
+
   const display = {
     your: hand,
     cpu: cpu,
