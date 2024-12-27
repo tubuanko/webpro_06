8b3251281805997b02873e9a77efc82bc673801a
tubuanko <tanishi0320@icloud.com>
2024-12-27 23:43:05 +0900

    プログラムを修正し機能に関する説明を追記した

--- app5.js
+++ app5.js
@@ -14,6 +14,61 @@ app.get("/hello2", (req, res) => {
   res.render('show', { greet1:"Hello world", greet2:"Bon jour"});
 });
 
+app.get("/icon", (req, res) => {
+  res.render('icon', { filename:"./public/Apple_logo_black.svg", alt:"Apple Logo"});
+});
+
+app.get("/luck", (req, res) => {
+  const num = Math.floor( Math.random() * 6 + 1 );
+  let luck = '';
+  if( num==1 ) luck = '大吉';
+  else if( num==2 ) luck = '中吉';
+  else if( num==3 ) luck = '吉';
+  else if( num==4 ) luck = '小吉';
+  else if( num==5 ) luck = '末吉';
+  else if( num==6 ) luck = '凶';
+  res.render( 'luck', {number:num, luck:luck} );
+});
+
+app.get("/janken", (req, res) => {
+  let hand = req.query.hand;
+  let win = Number( req.query.win );
+  let total = Number( req.query.total );
+  console.log( {hand, win, total});
+  const num = Math.floor( Math.random() * 3 + 1 );
+  let cpu = '';
+  if( num==1 ) cpu = 'グー';
+  else if( num==2 ) cpu = 'チョキ';
+  else if( num==3 ) cpu = 'パー';
+  // ここに勝敗の判定を入れる
+
+  let judgement = '';
+  if (
+    (hand === 'グー' && cpu === 'チョキ'),
+    (hand === 'チョキ' && cpu === 'パー'),
+    (hand === 'パー' && cpu === 'グー')
+  ) {
+    judgement = '勝ち';
+    win += 1;
+  }
+  else if (hand === cpu) {
+    judgement = '引き分け';
+  } 
+  else {
+    judgement = '負け';
+  }
+  total += 1;
+
+  const display = {
+    your: hand,
+    cpu: cpu,
+    judgement: judgement,
+    win: win,
+    total: total
+  }
+  res.render( 'janken', display );
+});
+
 app.get("/suziate", (req, res) => {
   let sainome = req.query.sainome;
   let win = Number( req.query.win );
@@ -56,44 +111,6 @@ app.get("/seiza", (req, res) => {
   else if( num==3 ) seiza = '悪いことがありそう';
   else if( num==4 ) seiza = '最悪の1日';
   res.render( 'seiza', {number:num, seiza:seiza} );
-});
-
-
-app.get("/janken", (req, res) => {
-  let hand = req.query.hand;
-  let win = Number( req.query.win );
-  let total = Number( req.query.total );
-  console.log( {hand, win, total});
-  const num = Math.floor( Math.random() * 3 + 1 );
-  let cpu = '';
-  if( num==1 ) cpu = 'グー';
-  else if( num==2 ) cpu = 'チョキ';
-  else if( num==3 ) cpu = 'パー';
-  // ここに勝敗の判定を入れる
-
-  let judgement = '';
-  if (hand === cpu) {
-    judgement = '引き分け';
-  } else if (
-    (hand === 'グー' && cpu === 'チョキ') 
-    (hand === 'チョキ' && cpu === 'パー') 
-    (hand === 'パー' && cpu === 'グー')
-  ) {
-    judgement = '勝ち';
-    win += 1;
-  } else {
-    judgement = '負け';
-  }
-  total += 1;
-
-  const display = {
-    your: hand,
-    cpu: cpu,
-    judgement: judgement,
-    win: win,
-    total: total
-  }
-  res.render( 'janken', display );
 });
 
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
